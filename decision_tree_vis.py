30b62c8cdff05f52de1955d6e513222687a7172d
Gurunath L V <gurunathrajagopal@gmail.com>
2018-09-27 18:11:52 +0530

    adding rf1

--- decision_tree_vis.py
+++ decision_tree_vis.py
@@ -3,3 +3,16 @@ from sklearn import tree
 from animl.viz.trees import dtreeviz
 from animl.trees import *
 import graphviz
+
+regr = tree.DecisionTreeRegressor(max_depth=2)
+boston = load_boston()
+regr.fit(boston.data, boston.target)
+
+viz = dtreeviz(regr,
+               boston.data,
+               boston.target,
+               target_name='price',
+               feature_names=boston.feature_names)
+              
+viz.view()              
+
